f69c13d70a127209592b077f3e6a31f0e35b3f8f
Dalton Wood <wood.dalton27@gmail.com>
2025-12-22 20:50:12 -0600

    animating home-page hero complete

---
+++ src/app/features/home/hero/hero.ts
@@ -0,0 +1,71 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  templateUrl: './hero.html',
+  styleUrl: './hero.scss',
+})
+export class Hero implements AfterViewInit {
+  @ViewChild('heroContainer') heroContainer!: ElementRef;
+
+  ngAfterViewInit(): void {
+    const container = this.heroContainer.nativeElement as HTMLElement;
+    const c = gsap.utils.selector(this.heroContainer.nativeElement);
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: container,
+        start: 'top top',
+        end: '+=1500',
+        pin: true,
+        scrub: 2,
+        markers: false,
+      },
+    });
+
+    tl.to(container, {
+      '--before-x': '1',
+      duration: 1,
+    });
+    tl.to(
+      container,
+      {
+        '--before-x': '10',
+        duration: 0,
+      },
+      '>'
+    );
+    tl.to(
+      c('.first-name'),
+      {
+        // xPercent: -92,
+        paddingRight: '120%',
+        // justifyContent: 'center',
+        duration: 2,
+      },
+      '<'
+    );
+    tl.to(
+      c('.last-name'),
+      {
+        // xPercent: 90,
+        paddingLeft: '120%',
+        // justifyContent: 'center',
+        duration: 2,
+      },
+      '<'
+    );
+    tl.to(
+      container,
+      {
+        '--before-x': '0',
+        duration: 0.5,
+      },
+      '-=.8'
+    );
+  }
+}
---
+++ src/app/features/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero/hero';
+
+@Component({
+  selector: 'app-home',
+  imports: [Hero],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {}
