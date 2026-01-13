b5687afc0f4efc8c37d85a00dcc4a93e767e6f6b
Dalton Wood <wood.dalton27@gmail.com>
2026-01-12 19:17:31 -0600

    adding actual images for gallery and horizontal scroll animation

--- src/app/features/home/gallery/gallery.ts
+++ src/app/features/home/gallery/gallery.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-gallery',
@@ -7,4 +11,23 @@ import { NgOptimizedImage } from '@angular/common';
   templateUrl: './gallery.html',
   styleUrl: './gallery.scss',
 })
-export class Gallery {}
+export class Gallery implements AfterViewInit {
+  @ViewChild('galleryContainer') galleryContainer!: ElementRef;
+
+  ngAfterViewInit(): void {
+    const container = this.galleryContainer.nativeElement as HTMLElement;
+
+    gsap.to(container, {
+      xPercent: -560,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: container,
+        start: 'top top',
+        end: 'bottom top',
+        pin: true,
+        scrub: 2,
+        // markers: true,
+      },
+    });
+  }
+}
